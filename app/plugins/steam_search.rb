23955dfeb59f5b7838c788fc5e2dec14ab97d346
Patrick Auernig <patrick.auernig@mykolab.com>
2017-06-06 22:06:20 +0200

    Include steam discount in price listing

--- app/plugins/steam_search.rb
+++ app/plugins/steam_search.rb
@@ -25,7 +25,8 @@ module SteamSearch
       embed.url = "#{STEAM_BASE_URL}?term=#{CGI.escape(query)}"
       search(query).take(5).each do |result|
         title = "[#{result.title}](#{result.link})"
-        embed.add_field(name: result.price, value: title)
+        price = result.discount.empty? ? result.price : "#{result.price} (#{result.discount})"
+        embed.add_field(name: price, value: title)
       end
     end
   end
@@ -43,7 +44,7 @@ module SteamSearch
         title: elem.css(".search_name > .title").text,
         release_date: date.empty? ? nil : DateTime.parse(date),
         reviews: elem.css(".search_review_summary").first&.attr("data-store-tooltip"),
-        price: elem.css(".search_price").first.text.strip,
+        price: elem.css(".search_price").children.last.text.strip,
         discount: elem.css(".search_discount").first.text.strip
       )
     end
