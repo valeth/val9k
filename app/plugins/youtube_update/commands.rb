1ca217f94cfb4383ab0641baa0043d5a37d19652
Patrick Auernig <patrick.auernig@mykolab.com>
2018-03-13 14:34:15 +0100

    Add youtube channels by name search

--- app/plugins/youtube_update/commands.rb
+++ app/plugins/youtube_update/commands.rb
@@ -1,11 +1,16 @@
 # frozen_string_literal: true
 
+require "active_support/core_ext/hash/keys"      # symbolize_keys
+require "active_support/core_ext/string/filters" # truncate
+require "rest-client"
 require_relative "subscription_scheduler"
 
 module YoutubeUpdate
   module Commands
     extend Discordrb::Commands::CommandContainer
     extend Utils
+
+    DiscordEmbedFooter = Discordrb::Webhooks::EmbedFooter
 
     command(
       %i[youtubeupdates ytupd],
@@ -26,22 +31,54 @@ module YoutubeUpdate
       %i[addyoutubeupdate addytupd],
       required_permissions: %i[manage_webhooks],
       min_args: 2,
-      max_args: 2,
-      usage: "addyoutubeupdate UCtxoI129gkBWW8_kNgJrxdQ #youtube_updates"
-    ) do |event, yt_cid, chan_mention, *args|
+      usage: "addyoutubeupdate ChannelName #youtube_updates"
+    ) do |event, *yt_channel, chan_mention|
       update_cid = parse_channel_mention(chan_mention)
       next "Channel mention required as second argument" unless update_cid
 
       begin
-        chan = SubscriptionScheduler.schedule(yt_cid)
-        YoutubeNotificationSubscription.create do |m|
-          m.youtube_channel = chan
-          m.discord_channel = DiscordChannel.find_by(cid: update_cid)
+        channels = youtube_channel_id(yt_channel.join(" "))
+        event.channel.send_embed do |embed|
+          embed.title = "YouTube search results"
+          embed.description = "#{channels.size} channels"
+          embed.footer = DiscordEmbedFooter.new(text: "Input a number to select a channel")
+          channels.each.with_index(1) do |chan, i|
+            desc = chan[:description].empty? ? "No description" : chan[:description].truncate(200)
+            embed.add_field(
+              name: "#{i}. #{chan[:title]}",
+              value: "[#{desc}](https://youtube.com/channel/#{chan[:id]})"
+            )
+          end
         end
+
+        event.author.await(:"addyoutubeupdate_#{event.author.id}") do |choice_event|
+          choice = choice_event.message.content
+
+          if choice.match?(/^\d+$/)
+            choice = choice.to_i
+          else
+            choice_event.send_temporary_message("That's not a number", 5)
+            next
+          end
+
+          unless choice.between?(1, channels.size - 1)
+            choice_event.send_temporary_message("Choice not in range", 5)
+            next
+          end
+
+          yt_cid = channels.dig(choice.pred, :id)
+          chan = SubscriptionScheduler.schedule(yt_cid)
+
+          YoutubeNotificationSubscription.create do |m|
+            m.youtube_channel = chan
+            m.discord_channel = DiscordChannel.find_by(cid: update_cid)
+          end
+          choice_event.send_message("Added #{chan_mention} to update channel list for `#{chan.name}`")
+        end
+
+        nil
       rescue SubscriptionScheduler::SubscriptionFailed
         "Failed to add subscription"
-      else
-        "Added #{chan_mention} to update channel list for `#{chan.name}`"
       end
     end
 
@@ -50,7 +87,7 @@ module YoutubeUpdate
       required_permissions: %i[manage_webhooks],
       min_args: 2,
       max_args: 2,
-      usage: "delyoutubeupdate UCtxoI129gkBWW8_kNgJrxdQ #youtube_updates"
+      usage: "delyoutubeupdate ChannelName #youtube_updates"
     ) do |event, yt_cid, chan_mention, *args|
       update_cid = parse_channel_mention(chan_mention)
       next "Channel mention required as second argument" unless update_cid
@@ -86,6 +123,21 @@ module YoutubeUpdate
         end
         tmp.compact.join("\n")
       end
+    end
+
+  module_function
+
+    # @param channel_name [String]
+    # @return [Array<Hash>] The channel ID
+    def youtube_channel_id(channel_name)
+      results = RestClient.get("#{SubscriptionScheduler::WEBSUB_URL}/search",
+        params: { channel_name: channel_name }
+      )
+
+      channels = JSON.parse(results)
+      raise SubscriptionScheduler::SubscriptionFailed if channels.empty?
+
+      channels.map { |chan| chan.symbolize_keys }
     end
   end
 end
