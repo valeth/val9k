15dd88abd3a8ab78d9798305be3f9c18c73bc19e
Patrick Auernig <patrick.auernig@mykolab.com>
2018-04-20 19:18:40 +0200

    Log server joins/leaves and channel creation

--- app/events.rb
+++ app/events.rb
@@ -15,6 +15,7 @@ module Events
   end
 
   server_create do |event|
+    LOGGER.info { "Joined #{event.server.name} (#{event.server.id})" }
     DiscordChannel.transaction do
       event.server.channels.each do |channel|
         DiscordChannel.create(sid: event.server.id, cid: channel.id)
@@ -23,14 +24,34 @@ module Events
   end
 
   server_delete do |event|
+    LOGGER.info { "Left #{event.server.name} (#{event.server.id})" }
     DiscordChannel.where(sid: event.server.id).destroy
   end
 
   channel_create do |event|
-    DiscordChannel.create(sid: event.server.id, cid: event.id)
+    LOGGER.info do
+      [
+        "New", channel_type_str(event.type), "channel",
+        "@", event.server.name, "(#{event.server.id})",
+        "#", event.channel.name, "(#{event.channel.id})"
+      ].compact.join(" ")
+    end
+    DiscordChannel.create(sid: event.server.id, cid: event.channel.id)
   end
 
   channel_delete do |event|
     DiscordChannel.find_by(sid: event.server.id, cid: event.id).destroy
+  end
+
+module_function
+
+  def channel_type_str(channel_type)
+    case channel_type
+    when 0 then "text"
+    when 1 then "private"
+    when 2 then "voice"
+    when 3 then "group"
+    else nil
+    end
   end
 end
