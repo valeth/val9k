3ce02d46ad3d85c881ca5980aea587c9ec1b17b9
Patrick Auernig <patrick.auernig@mykolab.com>
2018-04-20 18:54:42 +0200

    Replace faulty database update code

--- app/events.rb
+++ app/events.rb
@@ -12,71 +12,25 @@ module Events
     game = "#{bot.prefix}help"
     LOGGER.info { "Setting game to #{game}" }
     bot.game = game
-
-    update_channel_database(event.bot)
-  end
-
-  channel_create do |event|
-    update_channel_database(event.bot, action: :create)
-  end
-
-  channel_delete do |event|
-    update_channel_database(event.bot, action: :delete)
   end
 
   server_create do |event|
-    update_channel_database(event.bot, action: :create)
+    DiscordChannel.transaction do
+      event.server.channels.each do |channel|
+        DiscordChannel.create(sid: event.server.id, cid: channel.id)
+      end
+    end
   end
 
   server_delete do |event|
-    update_channel_database(event.bot, action: :delete)
+    DiscordChannel.where(sid: event.server.id).destroy
   end
 
-  module_function
-
-  # TODO: optimize this
-  def channels_in_db
-    DiscordChannel.all.map { |x| [x.sid, x.cid] }
+  channel_create do |event|
+    DiscordChannel.create(sid: event.server.id, cid: event.id)
   end
 
-  # TODO: optimize this
-  def channels_from_bot(bot)
-    bot.servers.flat_map do |sid, srv|
-      srv.channels.map { |x| [sid, x.id] }
-    end
-  end
-
-  def update_channel_database(bot, action: nil)
-    ch_local, ch_remote = channels_in_db, channels_from_bot(bot)
-
-    case action
-    when :create
-      fill_channel_database(ch_local, ch_remote)
-    when :delete
-      cleanup_channel_database(ch_local, ch_remote)
-    else
-      fill_channel_database(ch_local, ch_remote)
-      cleanup_channel_database(ch_local, ch_remote)
-    end
-  end
-
-  def cleanup_channel_database(channels_local, channels_remote)
-    only_local = channels_local - channels_remote
-
-    DiscordChannel.transaction do
-      only_local.each do |sid, cid|
-        DiscordChannel.find_by(sid: sid, cid: cid).destroy
-      end
-    end
-  end
-
-  def fill_channel_database(channels_local, channels_remote)
-    only_remote = channels_remote - channels_local
-
-    DiscordChannel.transaction do
-      only_remote.each do |sid, cid|
-        DiscordChannel.create(sid: sid, cid: cid)
-      end
-    end
+  channel_delete do |event|
+    DiscordChannel.find_by(sid: event.server.id, cid: event.id).destroy
   end
 end
