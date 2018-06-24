c4c787b3f35ac82c08f54c176327c476b18e60e6
Patrick Auernig <patrick.auernig@mykolab.com>
2018-06-24 03:53:29 +0200

    Use custom logger for Discordrb

--- app/application_logger.rb
+++ app/application_logger.rb
@@ -1,14 +1,25 @@
 require "logging"
+require "discordrb"
+require "active_support/core_ext/kernel/reporting"
 
 # STDOUT.sync = true
 
+LOGLEVELS = {
+  debug: :lime,
+  info: :green,
+  warn: :yellow,
+  error: :red,
+  fatal: %i[white on_red],
+  exception: %i[white on_red],
+  good: :green,
+  out: :cyan,
+  in: :purple
+}.freeze
+
+Logging.init(LOGLEVELS.keys)
+
 Logging.color_scheme("bright",
-  levels: {
-    info: :green,
-    warn: :yellow,
-    error: :red,
-    fatal: %i[white on_red]
-  },
+  levels: LOGLEVELS,
   date: :blue,
   logger: :cyan,
   message: :magenta
@@ -17,7 +28,7 @@ Logging.color_scheme("bright",
 Logging.appenders.stdout(
   "stdout",
   layout: Logging.layouts.pattern(
-    pattern: "[%-5l] %d : %m\n",
+    pattern: "[%-5l] %d %c: %m\n",
     color_scheme: "bright"
   )
 )
@@ -25,3 +36,22 @@ Logging.appenders.stdout(
 LOGGER = Logging.logger.root
 LOGGER.level = :debug
 LOGGER.appenders = :stdout
+
+class DiscordLogger < Discordrb::Logger
+  def initialize
+    super
+    @logger = Logging.logger["Discord"]
+    @enabled_modes = %i[good info warn error ratelimit]
+  end
+
+  MODES.keys.each do |lvl|
+    define_method(lvl) do |msg|
+      @logger.public_send(lvl, msg) if @enabled_modes.include?(lvl)
+    end
+  end
+end
+
+silence_warnings do
+  Discordrb::LOGGER = DiscordLogger.new
+end
+
